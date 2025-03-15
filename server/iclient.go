aca91ebf8e5cb39230f0b9df69e731be29e6642a
Snw <snw5213@gmail.com>
2025-03-15 16:57:14 +0800

    net.Conn

--- server/iclient.go
+++ server/iclient.go
@@ -1,6 +1,8 @@
 package server
 
-type IClient[USERDATA any, CONN any, MT MsgType] interface {
+import "net"
+
+type IClient[USERDATA any, CONN net.Conn, MT MsgType] interface {
 	GetId() ClientID
 	GetUserData() USERDATA
 	GetConn() CONN
